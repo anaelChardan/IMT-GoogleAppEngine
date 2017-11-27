c05f0e054a358fdc509638826c30765d930c925b
Anaël Chardan [aka Nanou] <anael.chardan@gmail.com>
2017-11-27 08:47:44 +0100

    Cache

--- src/main/java/com/zenika/zencontact/resource/UserResource.java
+++ src/main/java/com/zenika/zencontact/resource/UserResource.java
@@ -1,8 +1,10 @@
 package com.zenika.zencontact.resource;
 
+import com.google.appengine.api.memcache.Expiration;
+import com.google.appengine.api.memcache.MemcacheService;
+import com.google.appengine.api.memcache.MemcacheServiceFactory;
 import com.google.gson.Gson;
 import com.zenika.zencontact.domain.User;
-import com.zenika.zencontact.persistence.datastore.UserDaoDataStore;
 import com.zenika.zencontact.persistence.objectify.UserDaoObjectify;
 
 import javax.servlet.annotation.WebServlet;
@@ -10,16 +12,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 // With @WebServlet annotation the webapp/WEB-INF/web.xml is no longer required.
 @WebServlet(name = "UserResource", value = "/api/v0/users")
 public class UserResource extends HttpServlet {
 
+    private static final Logger LOG = Logger.getLogger(UserResource.class.getName());
+    private static final String CONTACTS_CACHE_KEY = "com.zenika.zencontact.cache";
+    private static final MemcacheService cache = MemcacheServiceFactory.getMemcacheService();
+
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
-        response.setContentType("application/json; charset=utf-8");
-        response.getWriter().println(new Gson().toJsonTree(UserDaoObjectify.getInstance().getAll()).getAsJsonArray());
+        List<User> contacts = (List<User>) cache.get(CONTACTS_CACHE_KEY);
+        if (contacts.isEmpty()) {
+            contacts = UserDaoObjectify.getInstance().getAll();
+            boolean isCached = contacts.size() > 0
+                    && cache.put(
+                    CONTACTS_CACHE_KEY, contacts,
+                    Expiration.byDeltaSeconds(240), MemcacheService.SetPolicy.ADD_ONLY_IF_NOT_PRESENT);
+        } else {
+            LOG.log(Level.INFO, "Cache HITS");
+        }
+            response.setContentType("application/json; charset=utf-8");
+            response.getWriter().println(new Gson().toJsonTree(contacts));
     }
 
     @Override
